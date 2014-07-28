9fbc68d9742e7d9350bb25af96fafa35d6596aab
Kyle Emmerich <emmerich.kyle@gmail.com>
2014-07-27 21:07:35 -0700

    Adding bindings for GLFW, OGL, and zlib

---
+++ src/Bindings/zlib.lua
@@ -0,0 +1,18 @@
+local S, this = ...
+
+local ffi = require("ffi")
+local z_lib
+
+if (ffi.os == "Windows") then
+	z_lib = ffi.load(this.dir .. "/win32/zlib1.dll")
+else
+	z_lib = ffi.load("z")
+end
+
+ffi.cdef([[
+	unsigned long compressBound(unsigned long sourceLen);
+	int compress2(uint8_t *dest, unsigned long *destLen, const uint8_t *source, unsigned long sourceLen, int level);
+	int uncompress(uint8_t *dest, unsigned long *destLen, const uint8_t *source, unsigned long sourceLen);
+]])
+
+return z_lib
