e62c193ee4c41354c5399872294df907186c0ae8
Lucien Greathouse <me@lpghatguy.com>
2014-07-28 00:26:27 -0600

    The OOP update

--- main.lua
+++ main.lua
@@ -1,4 +1,16 @@
 local Coeus = require("src.Coeus")
-local Bindings = Coeus.Bindings
 
-print(Bindings.opengl) --should yield a table
+local OOP = Coeus.Utility.OOP
+
+local test_class = OOP:Class() {
+	x = 1
+}
+
+function test_class:_new()
+	print("I'm new!")
+
+	self.y = 6
+end
+
+local test_instance = test_class:New()
+print(test_instance.x, test_instance.y)
