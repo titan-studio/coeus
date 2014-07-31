6d6f429d83c30455f8ef26c6cee2c0110448dd15
Lucien Greathouse <me@lpghatguy.com>
2014-07-31 01:10:19 -0600

    stdio binding update

--- src/Coeus/Bindings/stdio_.lua
+++ src/Coeus/Bindings/stdio_.lua
@@ -15,9 +15,18 @@ struct _iobuf {
 };
 typedef struct _iobuf FILE;
 
+enum {
+	SEEK_CUR = 1,
+	SEEK_END = 2,
+	SEEK_SET = 0
+};
+
 FILE* fopen(const char* filename, const char* mode);
 int fclose(FILE* stream);
 size_t fread(void* ptr, size_t size, size_t count, FILE* stream);
+int fseek(FILE* stream, long int offset, int origin);
+long int ftell(FILE* stream);
+
 ]])
 
 return C
