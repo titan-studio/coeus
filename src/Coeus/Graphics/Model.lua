dde416c45eb99a85d1f9f4bbad03b98e5dd380b9
Kyle Emmerich <emmerich.kyle@gmail.com>
2014-11-11 15:58:26 -0600

    Model class is now an Actor subclass

--- src/Coeus/Graphics/Model.lua
+++ src/Coeus/Graphics/Model.lua
@@ -2,16 +2,19 @@ local Coeus 	= (...)
 local OOP		= Coeus.Utility.OOP
 local Table		= Coeus.Utility.Table
 
+local Actor = Coeus.World.Actor
+
 local ModelData = Coeus.Asset.Model.ModelData
 
-local Model = OOP:Class() {
+local Model = OOP:Class(Actor) {
 	Meshes = {},
 	Materials = {},
 	Joints = {},
 	Animations = {},
 }
 
-function Model:_new(data)
+function Model:_new(scene, data)
+	Actor._new(self, scene)
 	for i, v in ipairs(data.Meshes) do
 		local mesh = Mesh:New()
 		mesh:SetData(v)
@@ -22,6 +25,16 @@ function Model:_new(data)
 	self.Materials = Table.Copy(data.Materials)
 	self.Joints = Table.Copy(data.Joints)
 	self.Animations = Table.Copy(data.Animations)
+
+	local root_trans = Coeus.World.Component.Transform:New()
+	self:AddComponent(root_trans)
+
+	for i, v in ipairs(self.Meshes) do
+		local child = Actor:New(scene)
+		child:SetParent(self)
+		local child_trans = Coeus.World.Component.Transform:New()
+
+	end
 end
 
 return Model
